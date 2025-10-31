a70143eb74d872efef161f16a34c19c905da0801
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2025-11-01 00:50:29 +0530

    support component ui complete

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,9 +1,91 @@
-import React from 'react'
+import React from "react";
 
 const CreateTicket = () => {
   return (
-    <div>CreateTicket</div>
-  )
-}
+    <div className="container p-5 mb-5 mt-5" style={{ color: "#424242" }}>
+      <div>
+        <h4 style={{ lineHeight: "2.5" }}>
+          To create a ticket, know a relevant topic
+        </h4>
+      </div>
 
-export default CreateTicket
+      <div className="row" style={{ lineHeight: "1.5" }}>
+        <div className="col-md-4">
+          <h5 style={{ lineHeight: "3.5" }}>
+            {" "}
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Account opening
+          </h5>
+
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Create Zerodha account</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>KYC and documentation</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Charges and fees</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Account closure</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Getting started with Zerodha</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Other account issues</p>
+          </a>
+        </div>
+
+        <div className="col-md-4">
+          <h5 style={{ lineHeight: "3.5" }}>
+            <i class="fa fa-user" aria-hidden="true"></i>
+            Your Zerodha account
+          </h5>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Login and password issues</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Funds deposit and withdrawal</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Trading and demat account</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Account settings and preferences</p>
+          </a>
+        </div>
+
+        <div className="col-md-4">
+          <h5 style={{ lineHeight: "3.5" }}>
+            {" "}
+            <i class="fa fa-bar-chart" aria-hidden="true"></i>
+            Your Zerodha account
+          </h5>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Login and password issues</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Funds deposit and withdrawal</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Trading and demat account</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Account settings and preferences</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Kite API</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>GTT</p>
+          </a>
+          <a href="#" style={{ textDecoration: "none" }}>
+            <p>Trading FAQs</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateTicket;
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="container mt-5">
+      <div className="row mt-5" >
+        <div className="col-md-8 text-muted">
+          <h1>Support Portal</h1>
+        </div>
 
-export default Hero
+        <div className="col-md-4 text-end">
+          <button
+            className="btn btn-primary"
+          >
+            My Tickets
+          </button>
+        </div>
+      </div>
+
+      <div className="input-group mt-4">
+        <span className="input-group-text bg-white ">
+          <i className="fa fa-search text-muted" aria-hidden="true"></i>
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Eg. How do I open my account, How do I activate my acc..."
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default Hero;
